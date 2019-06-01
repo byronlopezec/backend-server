06bf986258c2b452caef523d35fa42df9af8fa9f
Buhobit <byron.lopez.begins@gmail.com>
2019-06-01 15:24:38 -0500

    Primera ruta y nodemon

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+// Iniciar servidor
+// node app
+
+// iniciar servidor Con nodemon: installarlo> npm install -g nodemon
+// nodemon app.js
+
 // Requires
 var express = require("express");
 
 //Inicialiar variables
 var app = new express();
 
-// Escuchar peticiones
+// rutas
+app.get("/", (request, response, next) => {
+  response
+    .status(200)
+    .json({ ok: true, message: "Peticion realizada exitosamente" });
+});
 
+// Escuchar peticiones
 app.listen(3000, "localhost", () => {
   console.log("Express Server puerto\x1b[32m %s \x1b[0m%s", "3000", "online");
 });
