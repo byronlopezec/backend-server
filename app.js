bad9d7304b18e3d798e05ab4888d3f9bbe48ad7e
Buhobit <byron.lopez.begins@gmail.com>
2019-06-01 17:03:11 -0500

    Estableciendo conexión entre Mongo y Node

--- app.js
+++ app.js
@@ -6,15 +6,20 @@
 
 // Requires
 var express = require("express");
+var mongosse = require("mongoose");
 
 //Inicialiar variables
 var app = new express();
 
+// Conexion a la base de datos
+mongosse.connection.openUri("mongodb://localhost:27017/hospitalDB", (err, conn) => {
+  if (err) throw error;
+  console.log("Conexion con la base de datos en el puerto\x1b[32m %s \x1b[0m%s", "27017", "online");
+});
+
 // rutas
 app.get("/", (request, response, next) => {
-  response
-    .status(200)
-    .json({ ok: true, message: "Peticion realizada exitosamente" });
+  response.status(200).json({ ok: true, message: "Peticion realizada exitosamente" });
 });
 
 // Escuchar peticiones
