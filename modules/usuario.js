dd9488a4808bdfebf85b4e5084b53d3a5ea82e6d
Buhobit <byron.lopez.begins@gmail.com>
2019-06-01 19:11:59 -0500

    Creando el modelo de usuarios - Node

---
+++ modules/usuario.js
@@ -0,0 +1,16 @@
+// Importar la libreria
+var mongoose = require("mongoose");
+// llamar a la clase Schema para definir un esquema de usuario
+var Schema = mongoose.Schema;
+
+// definicion del Schema
+
+var usuarioSchema = new Schema({
+  nombre: { type: String, required: [true, "El nombre es necesario"] },
+  email: { type: String, unique: true, required: [true, "El correo es necesario"] },
+  password: { type: String, required: [true, "La contraseña es necesaria"] },
+  img: { type: String, required: false },
+  role: { type: String, required: true, default: "USER_ROLE" }
+});
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
