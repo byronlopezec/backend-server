6c7a31396ccdd05be281ab5b11b2c70183940505
Buhobit <byron.lopez.begins@gmail.com>
2019-06-02 23:12:12 -0500

    Delete - Borrar un usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var usuarioModel = require("../models/usuario");
-var bcrypt = require("bcryptjs");
+var bcrypt = require("bcryptjs"); // ========== *** https://www.npmjs.com/package/bcryptjs ***
 
 var app = new express();
 
@@ -24,20 +24,70 @@ app.get("/", (request, response, next) => {
 // ========== *** Agregar nuevo usuario ***
 app.post("/", (request, response, next) => {
   var body = request.body;
+  // TODO: Validar password undefined!!!
   var usuario = new usuarioModel({ ...body, password: bcrypt.hashSync(body.password, 10) });
 
   usuario.save((err, usuarioSaved) => {
     if (err) {
-      return response.status(400).json({
+      return response.status(400).json({ ok: false, ...err });
+    }
+
+    response.status(201).json({ ok: true, body: usuarioSaved });
+  });
+});
+
+// ========== *** Actualizar usuario ***
+app.put("/:id", (request, response) => {
+  var id = request.params.id;
+
+  usuarioModel.findById(id, "nombre email role", (err, usuarioFound) => {
+    if (err) {
+      return response.status(500).json({ ok: false, message: "Error buscar usuario", errors: err });
+    }
+
+    if (!usuarioFound) {
+      return response.status(400).json({ ok: false, errors: "Usuario no encontrado!" });
+    }
+
+    var body = request.body;
+    usuarioFound.nombre = body.nombre;
+    usuarioFound.email = body.email;
+    usuarioFound.role = body.role;
+
+    usuarioFound.save((err, usuarioGuardado) => {
+      if (err) {
+        return response
+          .status(400)
+          .json({ ok: false, message: "Error al actualizar usuario", err });
+      }
+
+      response.status(200).json({ ok: true, body: usuarioGuardado });
+    });
+  });
+});
+
+// ========== *** Borrar usuario ***
+app.delete("/:id", (request, response) => {
+  var id = request.params.id;
+
+  usuarioModel.findByIdAndDelete(id, (err, usuarioBorrado) => {
+    if (err) {
+      return response.status(500).json({
         ok: false,
-        ...err
+        mensaje: "Error al eliminar usuario",
+        errors: { message: "Error al eliminar usuario" }
       });
     }
 
-    response.status(201).json({
-      ok: true,
-      body: usuarioSaved
-    });
+    if (!usuarioBorrado) {
+      return response.status(400).json({
+        ok: false,
+        mensaje: "Usuario no encontrado para eliminar",
+        errors: { message: "Usuario no encontrado para eliminar" }
+      });
+    }
+
+    response.status(200).json({ ok: true, usuario: usuarioBorrado });
   });
 });
 
